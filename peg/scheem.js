8be1171defc8f0f2baf8e6fb2fc2f6a0111e2ded
Ashish Hanwadikar <ahanwadi@gmail.com>
2012-04-29 11:43:05 -0700

    scheem parser

---
+++ peg/scheem.js
@@ -0,0 +1,17 @@
+var PEG = require('pegjs');
+var assert = require('assert');
+var fs = require('fs'); // for loading files
+
+// Read file contents
+var data = fs.readFileSync('scheem.peg', 'utf-8');
+// Show the PEG grammar file
+console.log(data);
+// Create my parser
+var parse = PEG.buildParser(data).parse;
+
+assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
+assert.deepEqual(parse("atom"), "atom");
+assert.deepEqual(parse("+"), "+");
+assert.deepEqual(parse("(+ x 3)"), ["+", "x", "3"]);
+assert.deepEqual(parse("(+ 1 (f x 3 y))"), 
+    ["+", "1", ["f", "x", "3", "y"]]);
