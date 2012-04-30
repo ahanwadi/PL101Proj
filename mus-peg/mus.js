c946e3e9a1b16942b7147c32d15e20fb2531d6a5
Ashish Hanwadikar <ahanwadi@gmail.com>
2012-04-29 20:39:02 -0700

    Added support rests

--- mus-peg/mus.js
+++ mus-peg/mus.js
@@ -9,6 +9,7 @@ console.log(data);
 // Create my parser
 var parse = PEG.buildParser(data).parse;
 assert.deepEqual(parse("[100]", "duration"), 100);
+assert.deepEqual(parse("[100]", "note"), { tag: "rest", duration: 100 });
 assert.deepEqual(parse("a4[100]", "note"), { tag: "note", pitch: "a4", dur:100});
 
 assert.deepEqual(parse("(a4[100], a3[200])"), { tag: "seq", left: { tag: "note", pitch: "a4", dur:100 }, right: { tag: "note", pitch: "a3", dur:200 } });
