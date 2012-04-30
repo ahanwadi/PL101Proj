8f7d9008df40a715087aa2269d9cea176a8c2532
Ashish Hanwadikar <ahanwadi@gmail.com>
2012-04-29 20:53:29 -0700

    Added better example for parallel composition

--- mus-peg/mus.js
+++ mus-peg/mus.js
@@ -17,3 +17,5 @@ assert.deepEqual(parse("(a4[100], a3[200])"), { tag: "seq", left: { tag: "note",
 assert.deepEqual(parse("(a4[100], a3[200] | a4[40])"), { tag: "seq", left: {tag: "note", pitch: "a4", dur: 100 }, right: { tag: "par", left: { tag: "note", pitch: "a3", dur:200 }, right: { tag: "note", pitch: "a4", dur:40 } } });
 
 assert.deepEqual(parse("(a4[100], a3[200] * 3)"), { tag: "seq", left: {tag: "note", pitch: "a4", dur: 100 }, right: { tag: "repeat", section: { tag: "note", pitch: "a3", dur:200 }, count: 3 } });
+
+assert.deepEqual(parse("(a4[100] | a3[200] | a2[8])"), { tag: "par", left: {tag: "note", pitch: "a4", dur: 100 }, right: { tag: "par", left: { tag: "note", pitch: "a3", dur:200 }, right: {tag: "note", pitch: "a2", dur: 8} } });
